36f2f06c1457c2be6318e26495489ce54be2d6fd
Amit <amitkumar328862019@gmail.com>
2026-01-09 23:35:27 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config(); // ✅ always at top
+import dotenv from 'dotenv'
+dotenv.config();
+const http = require("http");
+const app = require("./app");
+const { initializeSocket } = require("./socket");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+initializeSocket(server);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
